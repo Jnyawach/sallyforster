60b69908def4edea7ac9a6908040221c4e3a108d
Jnyawach <60853597+Jnyawach@users.noreply.github.com>
2022-12-05 09:40:53 +0300

    update-homepage

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,17 @@
 
 <template>
 <header>
-  <h1>Hello there</h1>
+  <div class="flex justify-end">
+    <div class="flex justify-between">
+        <h1>Logo</h1>
+        <div class="">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+           <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+           </svg>
+         </div>
+    </div>
+
+  </div>
 </header>
 </template>
 
